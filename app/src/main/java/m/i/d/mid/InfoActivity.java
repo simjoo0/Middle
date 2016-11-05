e0c90ff2b61450ddd3796799e27484359dcf8646
simjoo0 <simjoo0@naver.com>
2016-11-06 03:37:26 +0900

    11.6.일 SplashActivity 구현 & 인원과 목적 입력받는 view layout (기능 미구현)

---
+++ app/src/main/java/m/i/d/mid/InfoActivity.java
@@ -0,0 +1,17 @@
+package m.i.d.mid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jooyoung on 2016-11-05.
+ */
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_layout);
+    }
+}
---
+++ app/src/main/java/m/i/d/mid/SplashActivity.java
@@ -0,0 +1,28 @@
+package m.i.d.mid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by jooyoung on 2016-11-05.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler() , 3000); // 3초 후에 hd Handler 실행
+    }
+
+    private class splashhandler implements Runnable{
+        public void run() {
+            startActivity(new Intent(getApplication(), InfoActivity.class)); // 로딩이 끝난후 이동할 Activity
+            SplashActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
+        }
+    }
+
+}
